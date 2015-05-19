aa981d3f24ef80b7b7e4d15cebf6164d843c6355
zjh599245299 <599245299@qq.com>
2015-05-19 13:59:20 +0800

    user.sql

--- jfinal_demo_for_jsp/启动说明/blog.sql
+++ jfinal_demo_for_jsp/启动说明/blog.sql
@@ -15,3 +15,17 @@ INSERT INTO `blog` VALUES ('2', 'test 1', 'test 1');
 INSERT INTO `blog` VALUES ('3', 'test 2', 'test 2');
 INSERT INTO `blog` VALUES ('4', 'test 3', 'test 3');
 INSERT INTO `blog` VALUES ('5', 'test 4', 'test 4');
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `role` int(1) NOT NULL COMMENT '角色1:代表乘客  2:代表司机',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'zhoujh', '1', '1');
