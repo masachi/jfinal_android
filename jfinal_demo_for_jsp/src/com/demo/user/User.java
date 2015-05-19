50861a6fc3114439aabe6e63269bb8fc60135b84
zjh599245299 <599245299@qq.com>
2015-05-19 15:43:11 +0800

    1

--- jfinal_demo_for_jsp/src/com/demo/user/User.java
+++ jfinal_demo_for_jsp/src/com/demo/user/User.java
@@ -5,23 +5,26 @@ import java.util.List;
 import com.jfinal.plugin.activerecord.Model;
 
 @SuppressWarnings("serial")
-public class User extends Model<User>{
-	
+public class User extends Model<User> {
+
 	public static User user = new User();
-	
-	public int loginCheck(String userName, String password){
+
+	public int loginCheck(String userName, String password) {
 		int flag = 0;
-		List<User> list = user.find("select * from user where username=?",userName);
-		if(list != null && list.size() > 0) {
-			//存在
-			User result = user.findFirst("select * from user where username=? and password=?" , userName, password);
-			if(result != null){
-				flag = 1;	//登录成功
-			}else{
-				flag = 2;	//密码错误
+		List<User> list = user.find("select * from user where username=?",
+				userName);
+		if (list != null && list.size() > 0) {
+			// 存在
+			User result = user.findFirst(
+					"select * from user where username=? and password=?",
+					userName, password);
+			if (result != null) {
+				flag = 1; // 登录成功
+			} else {
+				flag = 2; // 密码错误
 			}
-		}else{
-			flag = 3;	//账号不存在
+		} else {
+			flag = 3; // 账号不存在
 		}
 		return flag;
 	}
